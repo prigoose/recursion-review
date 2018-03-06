3e3d7837e39e290b64eff0ffa10ac98435342835
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-05 16:16:10 -0800

    Working on children nodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];  
+  var body = document.body;
+  var children = body.children;
+  console.log(children);
+  for (var i=0; i < children.length; i++) {
+    console.log('hello', children[i].classList)
+    if (children[i].classList.contains(className)) {
+      result.push(children[i])
+    }
+  }
 };
