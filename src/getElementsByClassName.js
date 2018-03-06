7e17bfcc0e8ea1cad9bf831adc388166628afd60
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-05 16:26:16 -0800

    -Working through Get Elements by Class Name -Updated the 'fill me in' value on the relevant test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,11 +9,15 @@ var getElementsByClassName = function(className) {
   var result = [];  
   var body = document.body;
   var children = body.children;
-  console.log(children);
+  console.log('children', children)
   for (var i=0; i < children.length; i++) {
-    console.log('hello', children[i].classList)
-    if (children[i].classList.contains(className)) {
-      result.push(children[i])
+    console.log('children i', children[i]);
+    if(typeof children[i].classList !== "undefined") {
+      if (children[i].classList.contains(className)) {
+        result.push(children[i]);
+      }
+        getElementsByClassName(children[i]);
     }
   }
+  return result;
 };
