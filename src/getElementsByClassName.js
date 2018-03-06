b16cd90b4ade712f02d16287310740ed53959f2c
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-05 21:14:10 -0800

    Solved getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,17 +7,41 @@
 var getElementsByClassName = function(className) {
   // your code here
   var result = [];  
-  var body = document.body;
-  var children = body.children;
-  console.log('children', children)
-  for (var i=0; i < children.length; i++) {
-    console.log('children i', children[i]);
-    if(typeof children[i].classList !== "undefined") {
-      if (children[i].classList.contains(className)) {
-        result.push(children[i]);
+  // var body = document.body;
+  var recursiveFunc = function(node) {
+    // current node - does class list exist? if so, does className exist? Push to results
+    if (node.classList) {
+      if (node.classList.contains(className)) {
+        result.push(node);
+        console.log('result', result)
       }
-        getElementsByClassName(children[i]);
+    }
+    // if children node exists - do the same
+    var children = node.childNodes;
+    if (children) {
+      for (var i = 0; i < children.length; i++) {
+      recursiveFunc(children[i]);
+      }
     }
   }
+  recursiveFunc(document.body);
   return result;
-};
+}
+//     var children = node.childNodes;
+//     if(children !== undefined) {
+//       for (var i=0; i < children.length; i++) {
+//         var list = children[i].classList;
+//         // if(typeof list !== "undefined" && list.length > 0) {
+//           if (list && list.contains(className)) {
+//             result.push(children[i]);
+//           }
+//           recursiveFunc(children[i]);
+//         // }
+//       }
+//     }
+//   }
+//   recursiveFunc(document.body);
+//   console.log(result);
+//   return result;
+// }
+
